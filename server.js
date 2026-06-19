37c22444d229ff620a379936b7637f08ba83b21b
chahakshahcs5 <chahak.shah.cs@gmail.com>
2026-06-19 14:53:37 +0530

    fix request handling

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const http = require('http');
+
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-  res.end('Hello World');
-});
+  if (req.url === '/about') {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain; charset=utf-8',
+    });
 
-// add about route
-server.on('request', (req, res) => {
-  if (req.url === 'about') {
-    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-    res.end('About Page');
+    return res.end('About Page');
   }
+
+  res.writeHead(200, {
+    'Content-Type': 'text/plain; charset=utf-8',
+  });
+
+  res.end('Hello World');
 });
 
 server.listen(PORT, () => {
